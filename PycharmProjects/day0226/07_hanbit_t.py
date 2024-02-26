d15d6105da0bd87c2dbc98baf4ca5dad81475da7
jusunKim <155524612+jusunKim@users.noreply.github.com>
2024-02-26 17:51:13 +0900

    0226 python

--- PycharmProjects/day0226/07_hanbit_t.py
+++ PycharmProjects/day0226/07_hanbit_t.py
@@ -9,11 +9,11 @@ text1 = re.findall('<!-- 책 리스트 -->(.+?)<!-- //책 리스트 -->',text,re
 arr = re.findall('<img src="(.+)" alt="" class="thumb" />',text1[0]) #text1이 리스트니까 그의 0번째라는거
 i=1
 for row in arr:
-    content = requests.get(base+""+row).content
+    content = requests.get(base+row).content
     f = open("./Data/hanB/book_"+str(i)+".jpg","wb")
     f.write(content)
     f.close()
     print("book_"+str(i)+"을 저장했습니다")
     i+=1
-    
+
 print("저장 완료")
